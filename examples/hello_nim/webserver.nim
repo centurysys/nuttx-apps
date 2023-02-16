ac1679c9d98b44e37ee098843e6922736d8188c4
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2023-02-16 09:04:48 +0900

    examples: hello_nim: update to return memory informations (/proc/meminfo).
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- examples/hello_nim/webserver.nim
+++ examples/hello_nim/webserver.nim
@@ -1,11 +1,17 @@
 import std/asyncdispatch
 import std/strformat
+import std/strutils
 import jester
 
 router myrouter:
   get "/hello/@name":
+    GC_runOrc()
     let username = @"name"
-    resp &"Hello, {username} !"
+    var buf = newSeqOfCap[string](5)
+    buf.add(&"Hello, {username} !")
+    buf.add("--- Memory Information ---")
+    buf.add(readFile("/proc/meminfo"))
+    resp buf.join("\n").replace("\n", "<br/>")
 
 proc runserver(): Future[void] {.async.} =
   let port = 80.Port
